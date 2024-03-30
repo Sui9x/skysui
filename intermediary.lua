30395521d4e5ca99fa85a7cba9472fda7895095e
Sui9x <146069698+Sui9x@users.noreply.github.com>
2024-03-30 22:13:30 +0900

    Add files via upload

--- intermediary.lua
+++ intermediary.lua
@@ -1,1 +1,1 @@
-pcall(load(gg.makeRequest("https://raw.githubusercontent.com/Sui9x/skysui/main/SkySui6.1z.lua").content))
+pcall(load(gg.makeRequest("https://raw.githubusercontent.com/Sui9x/skysui/main/SkySui6.2z.lua").content))
